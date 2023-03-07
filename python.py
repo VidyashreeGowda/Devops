992d01b53b62ebbe69e82563869e8705ad5af941
VidyashreeGowda <123718842+VidyashreeGowda@users.noreply.github.com>
2023-03-07 06:10:38 -0500

    updated

--- python.py
+++ python.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import smtplib
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login("dbcpolarbear@gmail.com", "sdguyoyuvnnqdyhw")
+
+# message to be sent
+message = "Hello Surendra Sir, how you are doing??, Have a good Day"
+
+# sending the mail
+s.sendmail("dbcpolarbear@gmail.com", "surendra.edcs@gmail.com", message)
